d1263dfd4a41ee1e4059ff97be0761a8b2f0c505
Nataniel Zwolinski <zwolinskin@zsl230.local>
2024-11-27 09:10:27 +0100

    27.11.24

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using podstawy3g2.Models;
+using System.Collections.Immutable;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -7,43 +8,41 @@ namespace podstawy3g2.Controllers
 {
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            if(Car.cars.Count == 0)
+            if (Car.cars.Count == 0)
             {
-                Car.cars.Add(new Car { Id = 1, Brand = "BMW", Model = "x7" });
-                Car.cars.Add(new Car { Id = 2, Brand = "Audi", Model = "a6" });
-                Car.cars.Add(new Car { Id = 3, Brand = "Fiat", Model = "tipo" });
+                Car.cars.Add(new Car { Id = 1, Brand = "BMW", Model = "x7" , EngineId = 1});
+                Car.cars.Add(new Car { Id = 2, Brand = "Audi", Model = "a6", EngineId = 2 });
+                Car.cars.Add(new Car { Id = 3, Brand = "Fiat", Model = "tipo", EngineId = 3});
             }
-
-            if (Engine.list.Count == 0)
+            if(Engine.list.Count == 0)
             {
-                Engine.list.Add(new Engine {Id = 1, Name = "Diesel"});
-                Engine.list.Add(new Engine {Id = 2, Name = "Benzin"});
-                Engine.list.Add(new Engine {Id = 3, Name = "Hybrid"});
+                Engine.list.Add(new Engine { Id = 1, Name = "Diesel" });
+                Engine.list.Add(new Engine { Id = 2, Name = "Benzin" });
+                Engine.list.Add(new Engine { Id = 3, Name = "Hybrid" });
             }
         }
-
         public IActionResult Index()
         {
-            @ViewBag.EngineList = Engine.list;
-            @ViewBag.EngId = 0;
-
+            ViewBag.EngineList = Engine.list;
+            ViewBag.EngId = 0;
             return View(Car.cars);
         }
-
         [HttpPost]
         public IActionResult Index(int EngId)
         {
-            @ViewBag.EngineList = Engine.list;
-            @ViewBag.EngId = EngId;
+            ViewBag.EngineList = Engine.list;
+            ViewBag.EngId = EngId;
             var list = Car.cars.Where(x => x.EngineId == EngId).ToList();
+            if (list.Count == 0)
+                list = Car.cars;
             return View(list);
         }
-
         public IActionResult AddCar()
         {
             ViewBag.Engines = Engine.list.Select(x => new SelectListItem
@@ -53,7 +52,6 @@ namespace podstawy3g2.Controllers
             }).ToList();
             return View();
         }
-
         [HttpPost]
         public IActionResult AddCar(Car car)
         {
@@ -68,20 +66,20 @@ namespace podstawy3g2.Controllers
                 Car.cars.Remove(car);
             return RedirectToAction("Index", "Home");
         }
+
         public IActionResult UpdateCar(int id)
         {
             Car car = Car.cars.FirstOrDefault(x => x.Id == id);
-            if (car != null)
+            if(car !=null)
             {
                 ViewBag.Engines = Engine.list.Select(x => new SelectListItem
                 {
-                    Text = x.Name, 
+                    Text = x.Name,
                     Value = x.Id.ToString(),
                     Selected = x.Id == car.EngineId
                 }).ToList();
                 return View(car);
             }
-                
             return RedirectToAction("Index", "Home");
         }
         [HttpPost]
@@ -94,9 +92,9 @@ namespace podstawy3g2.Controllers
                 updateCar.Model = car.Model;
                 updateCar.EngineId = car.EngineId;
             }
-
-            return RedirectToAction("Index", "Home");   
+            return RedirectToAction("Index", "Home");
         }
+        
         public IActionResult Details(int id)
         {
             Car car = Car.cars.FirstOrDefault(x => x.Id == id);
@@ -107,7 +105,6 @@ namespace podstawy3g2.Controllers
             }
             return RedirectToAction("Index", "Home");
         }
-
         public IActionResult ShowCar(int engId)
         {
             var cars = Car.cars.Where(c => c.EngineId == engId).Select(c => c.Brand).Distinct().ToList();
@@ -115,6 +112,7 @@ namespace podstawy3g2.Controllers
             ViewBag.SelectedEngineId = engId;
             return View(cars);
         }
+
 
         public IActionResult Privacy()
         {
